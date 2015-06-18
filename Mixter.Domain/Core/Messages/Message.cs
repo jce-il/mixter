e24bbdc30d162a794c3f48255bf07580d18ff73a
unknown <nbraslav@nbraslav-MOBL1.ger.corp.intel.com>
2015-06-18 18:54:17 +0300

    Solution 1.1

--- Mixter.Domain/Core/Messages/Message.cs
+++ Mixter.Domain/Core/Messages/Message.cs
@@ -54,6 +54,7 @@ namespace Mixter.Domain.Core.Messages
 
         public void Delete(IEventPublisher eventPublisher, UserId deleter)
         {
+            PublishEvent(eventPublisher, new MessageDeleted(this.GetId()));
         }
 
         public MessageId GetId()
