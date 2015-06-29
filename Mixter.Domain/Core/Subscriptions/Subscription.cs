0d80eb28d6d9e5967319fc7b30b3bbb5a5cffcd0
havivha <havivha@post.jce.ac.il>
2015-06-29 19:23:24 +0300

    raise user follow event when followed

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -7,10 +7,42 @@ namespace Mixter.Domain.Core.Subscriptions
 {
     public class Subscription
     {
+        private readonly DecisionProjection _projection;
+
+        public Subscription(IDomainEvent[] events)
+        {
+            _projection = new DecisionProjection();
+
+            foreach (var evt in events)
+            {
+                _projection.Apply(evt);
+            }
+        }
+
         public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee)
         {
             SubscriptionId subId = new SubscriptionId(follower, followee);
             eventPublisher.Publish(new UserFollowed(subId));
+        }
+
+        public void Unfollow(IEventPublisher eventPublisher)
+        {
+            eventPublisher.Publish(new UserUnfollowed(_projection.Id));
+        }
+
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            public DecisionProjection()
+            {
+                AddHandler<UserFollowed>(setSubId);
+            }
+
+            public SubscriptionId Id { get; private set; }
+
+            private void setSubId(UserFollowed evt)
+            {
+                Id = evt.SubscriptionId;
+            }
         }
     }
 }
