ce84d7696d46368de37d8fb37acf1d84d5847ec4
no-choice <zaslavchik.eli@gmail.com>
2015-07-02 20:20:18 +0300

    stem 3.2 completed \o/

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -5,11 +5,46 @@ using Mixter.Domain.Identity;
 
 namespace Mixter.Domain.Core.Subscriptions
 {
-    public class Subscription
+   public class Subscription
     {
+        private readonly DecisionProjection _projection;
+
+        public Subscription(IDomainEvent[] events)
+        {
+            _projection = new DecisionProjection();
+
+            foreach (var evt in events)
+            {
+                _projection.Apply(evt);
+            }
+        }
+
         public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee)
         {
-            eventPublisher.Publish(new UserFollowed(new SubscriptionId(follower, followee)));
+            var userFollowed = new UserFollowed(new SubscriptionId(follower, followee));
+            eventPublisher.Publish(userFollowed);
+        }
+
+        public void Unfollow(IEventPublisher eventPublisher)
+        {
+            eventPublisher.Publish(new UserUnfollowed(_projection.Id));
+        }
+
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            public DecisionProjection()
+            {
+                AddHandler<UserFollowed>(When);
+            }
+
+            public SubscriptionId Id { get; private set; }
+
+            private void When(UserFollowed evt)
+            {
+                Id = evt.SubscriptionId;
+            }
         }
     }
+
+
 }
